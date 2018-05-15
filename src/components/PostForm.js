bca01e0b76923c56850e8e30586f386dadebdde2
Jesse Smith <JesseSmith3rd@outlook.com>
2018-05-15 08:39:55 -0400

    Add onChange to take in values for title, body

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 class PostForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      title:"",
+      body:""
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
   }
   render() {
     return (
@@ -12,12 +19,15 @@ class PostForm extends Component {
         <form>
           <div>
             <label>Title: </label><br />
-            <input type="text" name="title" />
+            <input type="text" name="title" onChange={this.onChange} value={this.state.title} />
           </div>
+          <br />
           <div>
             <label>Body: </label><br />
-            <textarea name="body" />
+            <textarea name="body" onChange={this.onChange} value={this.state.body}/>
           </div>
+          <br />
+          <button type="submit">Submit</button>
         </form>
       </div>
     );
