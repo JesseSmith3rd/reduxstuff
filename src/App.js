479e8de64ad0fdae7913ff5599ce7321010c7f59
Jesse Smith <JesseSmith3rd@outlook.com>
2018-05-23 06:28:16 -0400

    add redux, store, provider

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 class App extends Component {
   render() {
     return (
+      <Provider store={store}>
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -17,6 +22,7 @@ class App extends Component {
         <hr />
         <Posts />
       </div>
+      </Provider>
     );
   }
 }
