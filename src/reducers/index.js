136c6ef1d4b1f596694e3f9b86e092547bcc638f
Jesse Smith <JesseSmith3rd@outlook.com>
2018-06-06 21:26:02 -0400

    remove syntax errors

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,22 +1,6 @@
-<<<<<<< HEAD
 import { combinedReducers } from 'react-redux';
 import postReducer from './postReducer';
 
-export default combinedReducers({
-  posts:postReducer
-=======
-<<<<<<< HEAD
-import { combineReducers } from 'redux';
-import postReducer from './postReducers';
-
 export default combineReducers({
   posts: postReducer
-=======
-import { combineReducers } from "redux";
-import postReducer from "./postReducers";
-
-export default combineReducers({
-  posts:postReducer
->>>>>>> development
->>>>>>> 139309cac82713bfe71dc3da7d9db5d7cd8fec7c
 });
