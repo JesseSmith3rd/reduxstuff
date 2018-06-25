5ee9c44a8af79ad58d7ae50b7b9bebe69e80db5c
Jesse Smith <JesseSmith3rd@outlook.com>
2018-06-25 09:28:52 -0400

    update master branch with development branch

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions/postAction';
 
@@ -7,7 +8,7 @@ class Posts extends Component {
     this.props.fetchPosts();
   }
   render() {
-    const postItems = this.state.posts.map(post => (
+    const postItems = this.props.posts.map(post => (
       <div key={post.id}>
       <h3>{post.title}</h3>
       <p>{post.body}</p>
@@ -21,5 +22,13 @@ class Posts extends Component {
     );
   }
 }
+Posts.propTypes = {
+  fetchPosts: PropTypes.func.isRequired,
+  posts: PropTypes.array.isRequired
+}
 
-export default connect(null, { fetchPosts })(Posts);
+const mapStateToProps = state => ({
+  posts: state.posts.items
+});
+
+export default connect(mapStateToProps, { fetchPosts })(Posts);
--- src/store.js
+++ src/store.js
@@ -1,14 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { applyMiddleware, createStore, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
 
 const initialState = {};
 
-const middleware = {thunk};
+const middleware = { thunk };
 
 const store = createStore(
   rootReducer,
   initialState,
+<<<<<<< HEAD
   applyMiddleware(...middleware));
+=======
+  compose(
+  applyMiddleware(...middleware),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+);
+>>>>>>> development
 
 export default store;
